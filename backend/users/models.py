f1d7dae4d12aaf065e85bde1a62179dd623eb129
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-09-20 13:50:12 +0600

    Created users models for database. Where user will be separated by their roles and suppliers details also included here.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+#This is importing Users from contrib or the user who registered in given django authentication system 
+from django.contrib.auth.models import AbstractUser
+
+#This is where im making another model for users and making sure they are imported from authenticated user.This will define user role.
+class User(AbstractUser):
+  Role_Choices = [
+    ('buyer','Buyer'),
+    ('supplier','Supplier'),
+    ('admin','Admin'),
+  ]
+  role = models.CharField(max_length=10,choices=Role_Choices,default='buyer')
+
+class SupplierProfile (models.Model):
+  user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='supplier_profile')
+  company_name = models.CharField(max_length=200)
+  address = models.TextField()
+  phone = models.CharField(max_length=20, blank=True)
+  is_verified = models.BooleanField(default=False)
+  logo = models.ImageField(upload_to='suppliers/logos/', blank=True, null=True)
+
+  def __str__(self):
+    return self.company_name
