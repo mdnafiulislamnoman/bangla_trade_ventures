11154609c28724519f301e4ff495bf2de943bf24
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-09-20 14:16:26 +0600

    Created model for containing product details on databse and its images on server

--- backend/catalog/models.py
+++ backend/catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from users.models import SupplierProfile
+
+class Product (models.Model):
+  supplier = models.ForeignKey(SupplierProfile, on_delete = models.CASCADE, related_name = 'products')
+  name = models.CharField(max_length = 200)
+  description = models.TextField()
+  category = models.CharField(max_length=100)
+  price_per_unit = models.DecimalField(max_digits = 10, decimal_places = 2)
+  unit = models.CharField(max_length=50)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.name
+  
+class ProductImage (models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+  image = models.ImageField(upload_to = 'products/images')
+
+  def __str__(self):
+    return f"image for {self.product.name}"
