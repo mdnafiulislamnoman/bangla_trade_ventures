acc0b2ab1b519234336a982eb41c1d676d3680de
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-09-22 04:06:06 +0600

    Users viewset

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+from .serializers import UserSerializer
+from .permissions import IsNotAuthenticated
 
 # Create your views here.
+class RegisterView (generics.CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+  permission_classes = [IsNotAuthenticated]
+
+class LoginView (TokenObtainPairView):
+  permission_classes = [IsNotAuthenticated]
