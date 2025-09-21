af0f09c15e657721697f271f6dd772ac3cef9613
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-09-22 03:34:58 +0600

    Catalog viewset

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 # Create your views here.
+
+class ProductViewSet (viewsets.ReadOnlyModelViewSet):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  filter_backends = [DjangoFilterBackend,SearchFilter]
+  filterset_fields = ['category']
+  search_fields = ['name','description']
