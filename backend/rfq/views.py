7b6e1ccf41e6ecae94d4d96f53f5d40a821d6652
mdnafiulislamnoman <software.engr.nafiul@gmail.com>
2025-09-22 03:51:27 +0600

    Rfq viewset

--- backend/rfq/views.py
+++ backend/rfq/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import RFQ
+from .serializers import RFQSerializer
+from users.permissions import IsBuyer
 
 # Create your views here.
+
+class RFQCreateView (generics.CreateAPIView):
+  queryset = RFQ.objects.all()
+  serializer_class = RFQSerializer
+  permission_classes = [IsBuyer]
+
+  def perform_create(self, serializer):
+    serializer.save(buyer = self.request.user)
